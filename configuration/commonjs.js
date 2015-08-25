1490541a2850adb8be8433043d5baa8fc1e5f553
Markus Wolf <markus.wolf@sinnerschrader.com>
2015-08-25 12:14:22 +0200

    Created own commonjs task

---
+++ configuration/commonjs.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  'tasks': {
+    'bundles': true,
+    'patterns': true,
+    'static': true,
+    'cache': false
+  },
+  'patterns': {
+    'cache': {
+      'static': false
+    },
+    'formats': {
+      'jsx': {
+        'name': 'Script',
+        'transforms': ['react']
+      }
+    }
+  },
+  'transforms': {
+    'react': {
+      'outFormat': 'js'
+    }
+  }
+};
