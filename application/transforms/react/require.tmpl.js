2d208b6628c06785f0923a339ef2203eb6455569
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-08-27 16:20:53 +0200

    Remove compile artifacts from version control, we are on npm now

--- application/transforms/react/require.tmpl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-var template = "\nvar originalRequire = require;\nvar __localDependencies__ = {\n\t$$localDependencies$$\n};\nvar require = function(id) {\n\tif (__localDependencies__[id]) {\n\t\tvar moduleScope = {exports:{}};\n\t\t__localDependencies__[id](moduleScope, moduleScope.exports, require);\n\t\treturn moduleScope.exports;\n\t}\n\treturn originalRequire(id);\n}\n";
-
-exports["default"] = template;
-module.exports = exports["default"];
