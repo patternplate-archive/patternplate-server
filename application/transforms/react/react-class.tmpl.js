b87e980d7f87ec93781a9ef0ae44754c08009a61
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-08-27 16:20:53 +0200

    Remove compile artifacts from version control, we are on npm now

--- application/transforms/react/react-class.tmpl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-var template = "\nimport * as React from 'react';\n$$dependencies$$\n\nexport default class $$class-name$$ extends React.Component {\n\trender() {\n\t\t$$render-code$$\n\t}\n}\n";
-
-exports["default"] = template;
-module.exports = exports["default"];
