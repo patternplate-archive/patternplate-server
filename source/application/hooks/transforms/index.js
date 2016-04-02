104cca5bf7187fd7cd2e10e1019c625fd54c02ab
Mario Nebl <hello@herebecode.com>
2016-04-02 21:44:33 +0200

    chore(system): enable hot-swapping for transforms

--- source/application/hooks/transforms/index.js
+++ source/application/hooks/transforms/index.js
@@ -1,22 +1,11 @@
-import {
-	resolve
-} from 'path';
+import hotswap from 'hotswap';
+import {resolve} from 'path';
+import {find, omit} from 'lodash';
+import {resolve as resolvePackage} from 'try-require';
 
-import {
-	omit
-} from 'lodash';
+import {deprecation, ok, wait} from '../../../library/log/decorations';
 
-import {
-	resolve as resolvePackage
-} from 'try-require';
-
-import {
-	deprecation,
-	ok,
-	wait
-} from '../../../library/log/decorations';
-
-function loadTransform(path) {
+function loadTransform(name, path) {
 	const available = resolvePackage(path);
 
 	if (!available) {
@@ -24,7 +13,12 @@ function loadTransform(path) {
 	}
 
 	try {
-		return require(path);
+		const fn = require(path);
+		return {
+			fn: fn.default ? fn.default : fn,
+			path: available,
+			name
+		};
 	} catch (error) {
 		error.message = [
 			`Error while loading transform ${path}:`,
@@ -34,9 +28,73 @@ function loadTransform(path) {
 	}
 }
 
+function loadTansformFactories(transformNames, paths, application) {
+	return transformNames
+		.reduce((registry, transformName) => {
+			application.log.debug(wait`Looking up transform ${transformName}`);
+			// resolve from project or core
+			// project transforms take precedence
+			const localTransformPaths = paths
+				.map(path => resolve(path, transformName, 'index.js'));
+
+			const localTransformFactory = localTransformPaths
+				.map(localPath => loadTransform(transformName, localPath))
+				.filter(Boolean)[0];
+
+			if (localTransformFactory) {
+				application.log.debug(ok`Loaded local transform ${transformName}`);
+				return {
+					...registry,
+					[transformName]: localTransformFactory
+				};
+			}
+
+			// try to load from patternplate-transform-${transformName}
+			// if no local transform was found
+			const pkg = `patternplate-transform-${transformName}`;
+			const packageTransformFactory = loadTransform(transformName, pkg);
+
+			if (!packageTransformFactory) {
+				throw new Error(
+					`Could not load transform ${transformName}, tried: ${localTransformPaths}, ${pkg} npm`
+				);
+			}
+
+			application.log.debug(ok`Loaded package transform ${transformName}`);
+
+			return {
+				...registry,
+				[transformName]: packageTransformFactory
+			};
+		}, {});
+}
+
+function getModulePool(root) {
+	return root.children.reduce((registry, child) => {
+		return [
+			...registry,
+			{
+				id: child.id,
+				children: getModulePool(child)
+			}
+		];
+	}, []);
+}
+
+function flattenChildren(root) {
+	return root.children.reduce((registry, child) => {
+		return [
+			...registry,
+			child,
+			...flattenChildren(child)
+		];
+	}, []);
+}
+
 export default {
 	wait: true,
 	after: ['hooks:log:start:afer'],
+	factories: [],
 	async start(application) {
 		// side track the pattern configuration to be free of breaking changes,
 		// deprecate the use of the config anyhow
@@ -60,44 +118,26 @@ export default {
 		const transformNames = Object.keys(omit(this.configuration, ['path', 'options']));
 
 		// load all transforms factories
-		const transformFactories = transformNames
-			.reduce((registry, transformName) => {
-				application.log.debug(wait`Looking up transform ${transformName}`);
-				// resolve from project or core
-				// project transforms take precedence
-				const localTransformPaths = resolved
-					.map(path => resolve(path, transformName, 'index.js'));
+		const transformFactories = loadTansformFactories(
+			transformNames, resolved, application
+		);
 
-				const localTransformFactory = localTransformPaths
-					.map(loadTransform)
-					.filter(Boolean)[0];
+		const pool = getModulePool(module);
 
-				if (localTransformFactory) {
-					application.log.debug(ok`Loaded local transform ${transformName}`);
-					return {
-						...registry,
-						[transformName]: localTransformFactory
-					};
-				}
-
-				// try to load from patternplate-transform-${transformName}
-				// if no local transform was found
-				const pkg = `patternplate-transform-${transformName}`;
-				const packageTransformFactory = loadTransform(`patternplate-transform-${transformName}`);
-
-				if (!packageTransformFactory) {
-					throw new Error(
-						`Could not load transform ${transformName}, tried: ${localTransformPaths}, ${pkg} npm`
-					);
-				}
-
-				application.log.debug(ok`Loaded package transform ${transformName}`);
-
-				return {
+		// save resolved paths for hot swapping
+		this.factories = Object.entries(transformFactories)
+			.reduce((registry, entry) => {
+				const [, {name, path}] = entry;
+				const module = find(pool, {id: path}) || {children: []};
+				const children = flattenChildren(module).map(child => {
+					return {name, id: path, path: child.id};
+				});
+				return [
 					...registry,
-					[transformName]: packageTransformFactory
-				};
-			}, {});
+					{name, id: path, path},
+					...children
+				];
+			}, []);
 
 		// attach transform registry to application object for usage
 		application.transforms = Object.entries(transformFactories)
@@ -105,9 +145,25 @@ export default {
 				const [name, factory] = entry;
 				return {
 					...registry,
-					[name]: (factory.default ? factory.default : factory)(application)
+					[name]: factory.fn(application)
 				};
 			}, {});
+
+		hotswap.on('swap', path => {
+			application.log.debug(`Received swap for ${path}...`);
+			const swapped = find(this.factories, {path});
+			if (swapped) {
+				const {name, id} = swapped;
+				application.log.info(`Hot-swapping transform ${name}...`);
+				application.transforms[name] = require(id)(application);
+				application.log.info(`Hot-swapped transform ${name}.`);
+			}
+		});
+
+		hotswap.on('error', error => {
+			application.log.info(`Error while hot-swapping:`);
+			application.log.error(error);
+		});
 
 		return this;
 	}
