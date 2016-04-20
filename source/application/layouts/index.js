7f860765dee9ac4e176115b1c37eade230bd7dc0
Mario Nebl <hello@herebecode.com>
2016-04-20 11:33:58 +0200

    fix(server): filter undefined references from layout

--- source/application/layouts/index.js
+++ source/application/layouts/index.js
@@ -5,14 +5,20 @@ function layout(props) {
 			<title>${props.title}</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 			${props.reference.style
+				.filter(Boolean)
+				.filter(style => Boolean(style.uri))
 				.map(style => `<link rel="stylesheet" href="${style.uri}">`)
 				.join('\n')}
 		</head>
 		<body>
 			${props.content.markup
+				.filter(Boolean)
+				.filter(markup => Boolean(markup.content))
 				.map(markup => markup.content)
 				.join('\n')}
 			${props.reference.script
+				.filter(Boolean)
+				.filter(script => Boolean(script.uri))
 				.map(script => `<script src="${script.uri}"></script>`)
 				.join('\n')}
 		</body>
