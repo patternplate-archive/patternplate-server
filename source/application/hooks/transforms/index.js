131229c805d96bea11b4c3fa095a446a0597cdf2
Mario Nebl <hello@herebecode.com>
2016-05-11 17:38:40 +0200

    fix(server): simplify transform resolver
    closes #68

--- source/application/hooks/transforms/index.js
+++ source/application/hooks/transforms/index.js
@@ -1,88 +1,37 @@
+import {
+	join,
+	resolve as pathResolve
+} from 'path';
+
+import chalk from 'chalk';
+import ensureArray from 'ensure-array';
 import hotswap from 'hotswap';
-import {resolve} from 'path';
-import {find, omit} from 'lodash';
-import {sync as resolveSync} from 'resolve';
+import _, {find, partial, omit, throttle} from 'lodash';
+import nodeResolve from 'resolve';
 
 import {deprecation, ok, wait} from '../../../library/log/decorations';
 
-function resolvePackage(id) {
-	try {
-		return resolveSync(id, {
+function resolve(input) {
+	return new Promise(resolver => {
+		nodeResolve(input, {
 			basedir: process.cwd()
+		}, (err, result) => {
+			if (err) {
+				resolver(null);
+			}
+			resolver(result);
 		});
-	} catch (error) {
-		return null;
-	}
+	});
 }
 
-function loadTransform(name, path, loadModule) {
-	const transformName = `patternplate-transform-${name}`;
-	const availableFile = resolvePackage(path);
-	const availableModule = loadModule ?
-		resolvePackage(transformName) :
-		null;
-
-	const available = availableFile || availableModule;
-
-	if (!available) {
-		return null;
-	}
-
-	try {
-		const fn = require(available);
-		return {
-			fn: fn.default ? fn.default : fn,
-			path: available,
-			name
-		};
-	} catch (error) {
-		error.message = [
-			`Error while loading transform ${name} from ${path}:`,
-			error.message
-		].join('\n');
-		throw error;
-	}
-}
-
-function loadTansformFactories(transformNames, paths, application) {
-	return transformNames
-		.reduce((registry, transformName) => {
-			application.log.debug(wait`Looking up transform ${transformName}`);
-			// resolve from project or core
-			// project transforms take precedence
-			const localTransformPaths = paths
-				.map(path => resolve(path, transformName, 'index.js'));
-
-			const localTransformFactory = localTransformPaths
-				.map(localPath => loadTransform(transformName, localPath, false))
-				.filter(Boolean)[0];
-
-			if (localTransformFactory) {
-				application.log.debug(ok`Loaded local transform ${transformName}`);
-				return {
-					...registry,
-					[transformName]: localTransformFactory
-				};
-			}
-
-			// try to load from patternplate-transform-${transformName}
-			// if no local transform was found
-			const pkg = `patternplate-transform-${transformName}`;
-			const packageTransformFactory = loadTransform(transformName, pkg, true);
-
-			if (!packageTransformFactory) {
-				throw new Error(
-					`Could not load transform ${transformName}, tried: ${localTransformPaths}, ${pkg} npm`
-				);
-			}
-
-			application.log.debug(ok`Loaded package transform ${transformName}`);
-
-			return {
-				...registry,
-				[transformName]: packageTransformFactory
-			};
-		}, {});
+function flattenChildren(root) {
+	return root.children.reduce((registry, child) => {
+		return [
+			...registry,
+			child.id,
+			...flattenChildren(child)
+		];
+	}, []);
 }
 
 function getModulePool(root) {
@@ -97,89 +46,125 @@ function getModulePool(root) {
 	}, []);
 }
 
-function flattenChildren(root) {
-	return root.children.reduce((registry, child) => {
-		return [
-			...registry,
-			child,
-			...flattenChildren(child)
-		];
-	}, []);
+async function resolveTransform(name, search = []) {
+	// Search given local paths
+	const locals = await Promise.all(
+		search
+			.map(async path => resolve(join(path, name)))
+	);
+
+	const resolved =
+		// load from local file
+		locals.filter(Boolean)[0] ||
+		// load from npm
+		await resolve(`patternplate-transform-${name}`);
+
+	// yeay!
+	if (resolved) {
+		return {
+			name,
+			resolved
+		};
+	}
+
+	// Throw if we could not load locally or from npm
+	throw new Error([
+		`Could not load transform "${name}". Tried loading locally from:`,
+		`${search.join(', ')}.`,
+		`Tried loading from npm: patternplate-transform-${name}`
+	].join(' '));
 }
+
+const transformPathDeprecation = [
+	'patternplate-server.configuration.patterns.transformPath is deprecated.',
+	'Use patternplate-server.configuration.transforms.path instead'
+].join(' ');
 
 export default {
 	wait: true,
 	after: ['hooks:log:start:afer'],
-	factories: [],
+
 	async start(application) {
+		const {
+			configuration: config,
+			configuration: {
+				path: basePath
+			}
+		} = this;
+
+		const {
+			configuration: {
+				patterns: {transformPath}
+			}
+		} = application;
+
 		// side track the pattern configuration to be free of breaking changes,
 		// deprecate the use of the config anyhow
-		if (application.configuration.patterns.transformPath) {
-			application.log.warn(deprecation`patternplate-server.configuration.patterns.transformPath is deprecated. Use patternplate-server.configuration.transforms.path instead`);
+		if (transformPath) {
+			application.log.warn(deprecation`${transformPathDeprecation}`);
 		}
 
-		const transformPath = application.configuration.patterns.transformPath || this.configuration.path;
-		const transformPaths = Array.isArray(transformPath) ?
-			transformPath :
-			[transformPath];
+		// Get places to load transforms from
+		const transformPaths = ensureArray(transformPath || basePath)
+			.map(path => pathResolve(process.cwd(), path));
 
-		const resolved = transformPaths
-			.reduce((items, item) => {
-				return [
-					...items,
-					...application.runtime.cwds.map(cwd => resolve(cwd, item))
-				];
-			}, []);
+		// Get configured transforms
+		const names = Object.keys(omit(config, ['path', 'options']));
 
-		const transformNames = Object.keys(omit(this.configuration, ['path', 'options']));
+		// load factories
+		// - from transformPaths
+		// - from node_modules
+		const resolve = partial(resolveTransform, _, transformPaths);
+		const factories = await Promise.all(names.map(async name => {
+			const resolved = await resolve(name);
+			application.log.silly(ok`Resolved transform "${name}"`);
+			resolved.factory = require(resolved.resolved);
+			application.log.silly(ok`Loaded transform "${name}"`);
+			return resolved;
+		}));
 
-		// load all transforms factories
-		const transformFactories = loadTansformFactories(
-			transformNames, resolved, application
-		);
+		// initialize transform factories
+		application.transforms = factories.reduce((registry, {name, resolved, factory}) => {
+			application.log.silly(ok`Initializing transform "${name}" from ${resolved}`);
+			const transform = factory(application);
+			application.log.debug(ok`Initialized transform "${name}"`);
 
-		const pool = getModulePool(module);
+			return {
+				...registry,
+				[name]: transform
+			};
+		}, {});
 
-		// save resolved paths for hot swapping
-		this.factories = Object.entries(transformFactories)
-			.reduce((registry, entry) => {
-				const [, {name, path}] = entry;
-				const module = find(pool, {id: path}) || {children: []};
-				const children = flattenChildren(module).map(child => {
-					return {name, id: path, path: child.id};
-				});
-				return [
-					...registry,
-					{name, id: path, path},
-					...children
-				];
-			}, []);
+		const ids = factories.map(({resolved}) => resolved);
 
-		// attach transform registry to application object for usage
-		application.transforms = Object.entries(transformFactories)
-			.reduce((registry, entry) => {
-				const [name, factory] = entry;
-				return {
-					...registry,
-					[name]: factory.fn(application)
-				};
-			}, {});
+		if (application.runtime.env === 'development') {
+			application.log.debug(wait`Listening for changes to transform code...`);
 
-		hotswap.on('swap', path => {
-			application.log.debug(`Received swap for ${path}...`);
-			const swapped = find(this.factories, {path});
-			if (swapped) {
-				const {name, id} = swapped;
-				application.log.info(`Hot-swapping transform ${name}...`);
-				application.transforms[name] = require(id)(application);
-				application.log.info(`Hot-swapped transform ${name}.`);
-			}
-		});
+			hotswap.on('swap', throttle(path => {
+				// Get transforms affected by change
+				getModulePool(module)
+					// Filter top level modules matching loaded transforms
+					.filter(({id}) => ids.includes(id))
+					// Filter for modules matching the changed path
+					.filter(mod => {
+						const moduleIds = [mod.id, ...flattenChildren(mod)];
+						return moduleIds.includes(path);
+					})
+					// Get transform factory for module with changes
+					.map(({id}) => find(factories, ({resolved}) => resolved === id))
+					// Reload affected transforms
+					.forEach(({name, resolved}) => {
+						application.log.debug(wait`Reloading transform "${name}"... ${chalk.grey(`(${path})`)}`);
+						application.transforms[name] = require(resolved)(application);
+						application.log.debug(ok`Reloaded transform "${name}"`);
+					});
+			}, 2500, {trailing: false}));
 
-		hotswap.on('error', error => {
-			application.log.info(`Error while hot-swapping:`);
-			application.log.error(error);
-		});
+			hotswap.on('error', error => {
+				application.log.info(`Error while hot-swapping:`);
+				application.log.error(error);
+			});
+		}
 
 		return this;
 	}
