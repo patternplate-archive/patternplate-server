3f5531acf6c4f8f5a81c7c9e2cbeb93673b2c2be
Mario Nebl <hello@herebecode.com>
2016-09-17 15:37:49 +0200

    fix: normalize pattern.formats[ext]

--- source/application/hooks/patterns/index.js
+++ source/application/hooks/patterns/index.js
@@ -1,9 +1,11 @@
+import {merge} from 'lodash';
 import patternFactory, {Pattern} from './pattern';
 
 export default {
 	wait: true,
 	after: ['hooks:cache:start:after'],
 	async start(application) {
+		const config = application.configuration.patterns || {};
 		application.pattern = {
 			factory(...args) {
 				return patternFactory(...[...args, application.cache]);
@@ -11,6 +13,28 @@ export default {
 			class: Pattern
 		};
 
+		// Normalize patterns.formats[ext]
+		config.formats = normalizeFormats(config.formats);
 		return this;
 	}
 };
+
+function normalizeFormats(formats = {}) {
+	return Object.entries(formats)
+		.reduce((formats, entry) => {
+			const [extname, format] = entry;
+			const defaults = {
+				name: extname,
+				build: false,
+				importStatement: i => `import ${i}`,
+				transforms: []
+			};
+			formats[extname] = merge(defaults, {
+				build: format.build,
+				name: format.name,
+				importStatement: format.importStatement,
+				transforms: format.transforms
+			});
+			return formats;
+		}, {});
+}
