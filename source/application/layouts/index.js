576739239f9797d50008b68dc4069678add1ceec
Mario Nebl <hello@herebecode.de>
2017-05-15 19:29:00 +0200

    fix: allow relative references

--- source/application/layouts/index.js
+++ source/application/layouts/index.js
@@ -1,12 +1,24 @@
+import path from 'path';
+
+export default layout;
+
 function layout(props) {
+	const styleRefs = (props.reference.style || []).filter(isReference);
+	const scriptRefs = (props.reference.script || []).filter(isReference);
+
 	return `<!doctype html>
 	<html>
 		<head>
 			<title>${props.title}</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 			<link rel="icon" href="data:;base64,iVBORw0KGgo=">
-			${(props.reference.style || [])
+			${styleRefs
+				.filter(isAbsolute)
 				.map(style => `<link rel="stylesheet" href="/api/resource/${style.id}.css">`)
+				.join('\n')}
+			${styleRefs
+				.filter(isRelative)
+				.map(style => `<link rel="stylesheet" href="${style.id}">`)
 				.join('\n')}
 			${(props.content.style || [])
 				.map(style => style.wrap === false ? style.content : `<style>${style.content}</style>`)
@@ -19,8 +31,13 @@ function layout(props) {
 			${(props.content.markup || [])
 				.map(markup => markup.content)
 				.join('\n')}
-			${(props.reference.script || [])
+			${scriptRefs
+				.filter(isAbsolute)
 				.map(script => `<script src="/api/resource/${script.id}.js"></script>`)
+				.join('\n')}
+			${scriptRefs
+				.filter(isRelative)
+				.map(script => `<script src="${script.id}"></script>`)
 				.join('\n')}
 			${props.content.script || []
 				.filter(Boolean)
@@ -31,4 +48,15 @@ function layout(props) {
 	</html>
 	`;
 }
-export default layout;
+
+function isAbsolute(reference) {
+	return !isRelative(reference);
+}
+
+function isReference(reference) {
+	return 'id' in reference;
+}
+
+function isRelative(reference) {
+	return (reference.id || '').charAt(0) === '.';
+}
