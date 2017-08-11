5936ac8b1e6182f8a420bf67003c2dc5c8c20188
mario.nebl@sinnerschrader.com <hello@herebecode.de>
2017-08-11 12:13:41 +0200

    feat: read markdown in pattern tree

--- source/library/utilities/get-pattern-tree.js
+++ source/library/utilities/get-pattern-tree.js
@@ -1,12 +1,16 @@
 import path from 'path';
+import frontmatter from 'front-matter';
 import globby from 'globby';
 import json from 'load-json-file';
+import {merge} from 'lodash';
 import exists from 'path-exists';
+import remark from 'remark';
+import find from 'unist-util-find';
+import * as sander from 'sander';
+import throat from 'throat';
 
 const DEFAULT_MANIFEST = {
 	version: '1.0.0',
-	build: true,
-	display: true,
 	options: {}
 };
 
@@ -30,7 +34,6 @@ export async function getPatterns(base) {
 			const manifest = {...DEFAULT_MANIFEST, ...data};
 			return {id, path: file, manifest};
 		}));
-
 
 	return patterns.map(pattern => {
 		const id = path.dirname(pattern.id);
@@ -69,22 +72,23 @@ export async function getPatternTree(base) {
 	return treeFromPaths(await getPatterns(base));
 }
 
-function treeFromPaths(files) {
+async function treeFromPaths(files) {
 	const tree = {
 		id: 'root',
 		children: []
 	};
 
-	files.forEach(file => {
+	await Promise.all(files.map(throat(1, async file => {
 		const parts = file.path.split('/');
 		let level = tree;
 
-		parts.forEach((id, i) => {
+		return await Promise.all(parts.map(throat(1, async (id, i) => {
 			const existing = level.children.find(c => c.name === id);
 			const n = parts[i + 1];
+			const itemPath = parts.slice(0, i + 1);
 
 			if (!n) {
-				return;
+				return null;
 			}
 
 			const type = getType(n || id);
@@ -92,16 +96,24 @@ function treeFromPaths(files) {
 
 			if (existing) {
 				level = existing;
-				return;
+				return null;
 			}
 
+			const contentsPath = path.resolve(...['./patterns', ...itemPath, 'readme.md']);
+			const contents = await exists(contentsPath) ? String(await sander.readFile(contentsPath)) : '';
+			const ast = remark().parse(contents);
+			const first = find(ast, {type: 'heading', depth: 1});
+			const front = typeof contents === 'string' ? frontmatter(contents).attributes : {};
+			const manifest = merge({}, DEFAULT_MANIFEST, front);
+			manifest.name = first ? first.children[0].value : name;
+			manifest.displayName = manifest.displayName || manifest.name;
+
 			const item = {
+				contents,
 				name,
-				manifest: type === 'folder' ?
-					{displayName: id, name: id, options: {}} :
-					file.manifest,
+				manifest: type === 'folder' ? manifest : file.manifest,
 				id: parts.slice(0, i + 1).join('/'),
-				path: parts.slice(0, i + 1),
+				path: itemPath,
 				type
 			};
 
@@ -114,8 +126,10 @@ function treeFromPaths(files) {
 				item.dependents = file.dependents;
 				item.dependencies = file.dependencies;
 			}
-		});
-	});
+
+			return null;
+		})));
+	})));
 
 	return tree;
 }
